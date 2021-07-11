049601a53b4f1fe77bfb6d3274ad79edd5598908
Andriy Golumbovsky <agolumbovsky@gmail.com>
2021-07-11 10:25:28 -0400

    minor addition

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf('1887280803:AAGB0FzoGvfQ30FSlA648zd_1HuYSrvJ_Sc');
 
-bot.command('art', ctx => {
+bot.command('hey', ctx => {
     console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, "dafuq you lookin at", {
+    bot.telegram.sendMessage(ctx.chat.id, "dafuq you lookin at?", {
+
+    })
+})
+
+bot.command('love', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, "Fuck off, you hippie", {
 
     })
 })
