3015468112194343ccc8c3802c7ed75651e3d731
Andriy Golumbovsky <agolumbovsky@gmail.com>
2021-07-11 10:32:14 -0400

    polyfill for some stupid stuff

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const { Telegraf } = require('telegraf');
 
+if (!Object.entries)
+   Object.entries = function( obj ){
+      var ownProps = Object.keys( obj ),
+         i = ownProps.length,
+         resArray = new Array(i); // preallocate the Array
+
+      while (i--)
+         resArray[i] = [ownProps[i], obj[ownProps[i]]];
+      return resArray;
+   };
+
 const bot = new Telegraf('1887280803:AAGB0FzoGvfQ30FSlA648zd_1HuYSrvJ_Sc');
 
 bot.command('hey', ctx => {
